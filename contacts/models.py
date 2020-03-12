e86d5d73a8b2e42f330aa73859509b2ed2a5604f
Vahid Nasri <vahidffs@gmail.com>
2020-03-11 22:31:25 -0400

    Create contact app for inquiries

--- contacts/models.py
+++ contacts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 class Contact(models.Model):
     listing = models.CharField(max_length=200)
-    listing_id = models.IntegerField
+    listing_id = models.IntegerField()
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
